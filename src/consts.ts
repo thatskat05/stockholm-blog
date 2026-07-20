5eb5274842fe49839f16a67eff45e3be62908b22
Katherine <thatskat05@gmail.com>
2026-07-19 22:24:02 -0500

    retro blog with homepage and photo

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Katarina i Sverige';
+export const SITE_DESCRIPTION = 'Documenting my autumn exchange in Stockholm!';
